d80958f8202845db10de218f0923bd388fac01b9
Md Kaif <163404336+kaife0@users.noreply.github.com>
2025-02-11 12:55:21 +0530

    Update loops.js

--- arrays/loops.js
+++ arrays/loops.js
@@ -3,7 +3,7 @@ let a = [1, 44, 7, 55, 88]
 // for (let index = 0; index < a.length; index++) {
 //     const element = a[index];
 //     console.log(element);
-// }
+// } 
 
 // a.forEach((value, index, arr)=>{
 //     console.log(value, index, arr)
