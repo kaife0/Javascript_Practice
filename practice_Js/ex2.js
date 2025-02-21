d92f879256082b6e1e063ae31fd40be78912f814
Md Kaif <163404336+kaife0@users.noreply.github.com>
2025-02-21 11:35:02 +0530

    Update ex2.js

--- practice_Js/ex2.js
+++ practice_Js/ex2.js
@@ -3,7 +3,7 @@
 const makeNewString = (str)=> 
     str.length < 3 ? str : str.slice(0,3) + str.slice(-3);
 
-console.log(makeNewString('abc'));
+console.log(makeNewString('abc')); 
 console.log(makeNewString('abc123abc123'));
 console.log(makeNewString('ab'));
 
